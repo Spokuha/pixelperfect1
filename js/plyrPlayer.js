a78c3e8a3e2f34e127b445ff2415d56f0d234fb4
Pavel <p.petrunkin@dunice.net>
2019-07-12 18:01:50 +0300

    start block5

--- js/plyrPlayer.js
+++ js/plyrPlayer.js
@@ -2,14 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const controls = [
         'play-large', // The large play button in the center
-        'restart', // Restart playback
-        'rewind', // Rewind by the seek time (default 10 seconds)
-        'play', // Play/pause playback
-        'fast-forward', // Fast forward by the seek time (default 10 seconds)
-        'progress', // The progress bar and scrubber for playback and buffering
-        'current-time', // The current time of playback
-        'duration', // The full duration of the media
-        'fullscreen', // Toggle fullscreen
     ];
 
     const player = new Plyr('#player', { controls });
